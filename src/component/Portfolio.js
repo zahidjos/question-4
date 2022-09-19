333fdf01a1186179191c1ba31757b557bd18d917
zahidjos <zahidhasanjos90@gmail.com>
2022-09-19 10:56:46 +0600

    question-4

---
+++ src/component/Portfolio.js
@@ -0,0 +1,256 @@
+import React from 'react';
+
+const MyPortfolio = () => {
+    return (
+        <div>
+            
+
+<div class="card w-3/4 mt-5 mx-auto bg-base-100 shadow-xl">
+  <div class="card-body">
+    
+    <h3 class="card-title" >MD. ZAHID HASAN JOS</h3>
+   
+  </div>
+</div>
+{/* education start */}
+<div class="card w-3/4 mt-5 mx-auto bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">EDUCATION</h2>
+    
+    <div className='mt-5'>
+        
+        <div class="overflow-x-auto mt-5">
+  <table className="table w-full">
+    
+    <thead>
+      <tr>
+        
+        <th>World University Of Bangladesh</th>
+        <th></th>
+        
+      </tr>
+      
+    </thead>
+    <tbody>
+     
+      <tr>
+        <th>Program :  </th>
+        <td>B.Sc. in Mechatronics Engineering</td>
+        
+      </tr>
+      <tr>
+        <th>Passing Year :  </th>
+        <td>2022</td>
+        
+      </tr>
+      <tr>
+        <th>Result :  </th>
+        <td>CGPA– 3.39 out of 4.00 </td>
+        
+      </tr>
+      
+    </tbody>
+  </table>
+</div>
+    </div>
+    
+  </div>
+</div>
+{/* education end */}
+{/* profession  start */}
+<div class="card w-3/4 mt-5 mx-auto bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">PROFESSIONAL SKILL</h2>
+    <div class="overflow-x-auto">
+  <table class="table w-full">
+    
+    <thead>
+      <tr>
+        <th></th>
+        
+      </tr>
+    </thead>
+    <tbody>
+     
+      <tr>
+        <th><li>HTML</li></th>
+        
+      </tr>
+      <tr>
+        <th><li>CSS</li></th>
+        
+      </tr>
+      <tr>
+        <th><li>CSS3</li></th>
+        
+      </tr>
+      <tr>
+        <th><li>Bootstrap</li></th>
+        
+      </tr>
+      <tr>
+        <th><li>SASS</li></th>
+        
+      </tr>
+      <tr>
+        <th><li>J-query</li></th>
+        
+      </tr>
+      <tr>
+        <th><li>J-query Plugin</li></th>
+        
+      </tr>
+      <tr>
+        <th><li>React </li></th>
+        
+      </tr>
+      <tr>
+        <th><li>Node JS</li></th>
+        
+      </tr>
+      <tr>
+        <th><li>Express </li></th>
+        
+      </tr>
+      <tr>
+        <th><li>MongoDB</li></th>
+        
+      </tr>
+      
+
+   
+      
+     
+    </tbody>
+  </table>
+</div>
+    
+   
+  </div>
+</div>
+{/* profession  end */}
+{/* personal   start */}
+
+<div class="card w-3/4 mx-auto mt-5 bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">PERSONAL INFORMATION</h2>
+    <div class="overflow-x-auto">
+  <table class="table w-full">
+    
+    <thead>
+      <tr>
+        <th></th>
+        <th></th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      
+      <tr>
+        <th>Name:</th>
+        <td>Md. Zahid Hasan Jos</td>
+        </tr>
+
+        <tr>
+        <th>Father’s Name:</th>
+        <td>Md. Amis Uddin</td>
+        </tr>
+        <tr>
+        <th>Mother’s Name:</th>
+        <td>Most. Johora Nasrin</td>
+        </tr>
+
+        <tr>
+        <th>Date of Birth:</th>
+        <td>18/01/1998</td>
+        </tr>
+
+        <tr>
+        <th>Religion: </th>
+        <td>Islam</td>
+        </tr>
+
+        <tr>
+        <th>Nationality: </th>
+        <td>Bangladeshi</td>
+        </tr>
+
+        <tr>
+        <th>Marital Status:</th>
+        <td>Unmarried</td>
+        </tr>
+
+        <tr>
+        <th>Blood Group:</th>
+        <td>O+”(ve)”</td>
+        </tr>
+
+        <tr>
+        <th>NID No:</th>
+        <td>3754179376</td>
+        </tr>
+
+      
+    
+
+     
+    </tbody>
+  </table>
+</div>
+   
+  </div>
+</div>
+{/* personal   end */}
+{/* project   start */}
+
+<div class="card w-3/4 mt-5 mx-auto bg-base-100 shadow-xl">
+  <div class="card-body">
+    
+    <h3 class="card-title" >project Link</h3>
+    <div class="overflow-x-auto">
+  <table class="table w-full">
+    
+    <thead>
+      <tr>
+        <th>Project link</th>
+        
+        
+      </tr>
+    </thead>
+    <tbody>
+      
+      <tr>
+        <th>Service-provider</th>
+        <td><a className='cursor-pointer btn btn-outline btn-primary' target='_blank' href='https://service-provider-55df2.web.app'>Click</a></td>
+       
+        </tr>
+
+        <tr>
+        <th>Ware House</th>
+        <td><a  className="cursor-pointer btn btn-outline btn-primary" target='_blank' href='https://ware-house-e9fe3.firebaseapp.com/'>Click</a></td>
+        </tr>
+        <tr>
+        <th>Firebase Authentication</th>
+        <td><a className='cursor-pointer btn btn-outline btn-primary' target='_blank' href='https://fastidious-sherbet-d02054.netlify.app/'>Click</a></td>
+        </tr>
+
+        
+      
+    
+
+     
+    </tbody>
+  </table>
+</div>
+   
+  </div>
+</div>
+{/* project   end */}
+
+
+
+        </div>
+    );
+};
+
+export default MyPortfolio;
